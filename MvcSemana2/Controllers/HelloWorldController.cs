985f6936acd0b0be982950fb92b74d8cc9ef20b3
mati <matiaslchamarez@gmail.com>
2024-04-11 12:10:45 -0300

    Agregar archivos de proyecto.

---
+++ MvcSemana2/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Encodings.Web;
+
+namespace MvcSemana2.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/ Reemplazar el método index:
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/ Agregar este método:
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
---
+++ MvcSemana2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcSemana2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
